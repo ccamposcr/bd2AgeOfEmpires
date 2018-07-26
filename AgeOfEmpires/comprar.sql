5b59d60ce2941282f6ba4e304b31523ec0aebca7
CHRISTIANPC\Christian <ccamposcr@gmail.com>
2018-07-25 21:22:07 -0600

    Comprar

--- AgeOfEmpires/comprar.sql
+++ AgeOfEmpires/comprar.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE PROCEDURE comprar
    ( recurso varchar2 , reino varchar2, cantidad float )
 IS
     CURSOR cursor_reino is
-        SELECT IDReino, IDTesoro, cantidadDeMadera, cantidadDeHierro, cantidadDeOro
+        SELECT IDReino, IDTesoro, cantMadera, cantHierro, cantOro
         from Reino
         INNER JOIN Tesoro ON Reino.Tesoro_IDTesoro = Tesoro.IDTesoro
         where Reino.nombre = UPPER(reino);
@@ -19,13 +19,13 @@ BEGIN
         LOOP
             IF recurso = 'madera' THEN
                 totalCostoMadera := cantidad * indice2.precioMadera;
-               IF cantidad <= indice2.cantMadera AND totalCostoMadera <= indice.cantidadDeOro THEN 
+               IF cantidad <= indice2.cantMadera AND totalCostoMadera <= indice.cantOro THEN 
                     UPDATE Tesoro
-                    SET cantidadDeMadera = cantidadDeMadera + cantidad, cantidadDeOro = cantidadDeOro - totalCostoMadera
+                    SET cantMadera = cantMadera + cantidad, cantOro = cantOro - totalCostoMadera
                     WHERE idTesoro = indice.idTesoro;
                     
                     UPDATE Reino
-                    SET cantidadCoronas = cantidadCoronas + 5
+                    SET cantCoronas = cantCoronas + 5
                     WHERE idReino = indice.idReino;
                     
                     UPDATE ReservaCentral
@@ -36,13 +36,13 @@ BEGIN
                 END IF;
             ELSE
                 totalCostoHierro := cantidad * indice2.precioHierro;
-                IF cantidad <= indice2.cantHierro AND totalCostoHierro <= indice.cantidadDeOro THEN
+                IF cantidad <= indice2.cantHierro AND totalCostoHierro <= indice.cantOro THEN
                     UPDATE Tesoro
-                    SET cantidadDeHierro = cantidadDeHierro + cantidad, cantidadDeOro = cantidadDeOro - totalCostoHierro
+                    SET cantHierro = cantHierro + cantidad, cantOro = cantOro - totalCostoHierro
                     WHERE idTesoro = indice.idTesoro;
                     
                     UPDATE Reino
-                    SET cantidadCoronas = cantidadCoronas + 5
+                    SET cantCoronas = cantCoronas + 5
                     WHERE idReino = indice.idReino;
                     
                     UPDATE ReservaCentral
@@ -53,7 +53,7 @@ BEGIN
                 END IF;
             END IF;
             
-            INSERT into Bitacora values(bitacora_sequence.nextval, sysdate, indice.cantidadDeOro,indice.cantidadDeHierro,indice.cantidadDeMadera,0,'CMP', indice.IDReino);
+            INSERT into Bitacora values(bitacora_sequence.nextval, sysdate, indice.cantOro,indice.cantHierro,indice.cantMadera,0,'CMP', indice.IDReino);
             commit;
         END LOOP;
     END LOOP;
