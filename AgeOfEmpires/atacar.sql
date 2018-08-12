3b1c0e8ff7c21fc864749587285691130df635f1
adriancr93 <aobregonr@ucenfotec.ac.cr>
2018-08-12 00:04:03 -0600

    Avance de atacar

--- AgeOfEmpires/atacar.sql
+++ AgeOfEmpires/atacar.sql
@@ -1,22 +1,58 @@
 CREATE OR REPLACE PROCEDURE atacar
  ( reinoAtacante int, reinoDefensa int)
 IS
+  subTotalAtaque number := 0;
+  subTotalAtaqueBonus number := 0;
+  subTotalDefensa number :=0;
+  totalAtaque number := 0;
+  totalDefensa number := 0;
   CURSOR curataque(identAtaque int) IS
-     SELECT t.cantoro, r.puntosataque, r.puntosdefensa 
-     FROM reino r join tesoro t ON r.idreino = identAtaque;
-     cantoro t.cantoro%type;
-     puntosataque r.puntosataque%type;
-     puntosdefensa r.puntosdefensa%type;
+     SELECT cantOro, puntosAtaque, puntosDefensa 
+     FROM reino r JOIN tesoro t ON r.idreino = t.idtesoro 
+     WHERE idReino = identAtaque;
+     cantoro tesoro.cantOro%type;
+     puntosataque reino.puntosAtaque%type;
+     puntosdefensa reino.puntosDefensa%type;
+  CURSOR curdefensa(identDefensa int) IS 
+      SELECT cantOro, cantMadera, cantHierro, puntosAtaque, puntosDefensa
+      FROM reino r JOIN tesoro t On r.idreino = t.idtesoro
+      where idReino = identDefensa;
+      cantorodef tesoro.cantOro%type;
+      cantmaderadef tesoro.cantMadera%type;
+      canthierrodef tesoro.cantHierro%type;
+      puntosataquedef reino.puntosAtaque%type;
+      puntosdefensadef reino.puntosDefensa%type;
 BEGIN
      open curataque(reinoAtacante);
       LOOP
          fetch curataque into cantoro, puntosataque, puntosataque;
-          IF cantoro <= 1000 then
-               dbms_output.put_line(' El ataque es posible');
+         exit when curataque%notfound;
+          IF cantoro >= 1000 then
+               UPDATE tesoro
+               SET cantOro = cantoro - 1000
+               WHERE idtesoro = reinoAtacante;
+               subTotalAtaque :=  puntosataque * 80 / 100;
+               subTotalAtaqueBonus := puntosataque * 60 / 100;
+               totalAtaque := subTotalAtaque + subTotalAtaqueBonus;
+               dbms_output.put_line(totalAtaque);
+               open curdefensa(reinoDefensa);
+                LOOP
+                   fetch curdefensa into cantorodef, cantmaderadef, canthierrodef, puntosataquedef, puntosdefensadef;
+                   exit when curdefensa%notfound;
+                     totalDefensa := puntosdefensadef * 70 / 100;
+                     dbms_output.put_line(totalDefensa);
+                END LOOP;
+               close curdefensa;
+               IF totalAtaque > totalDefensa THEN
+                 dbms_output.put_line('El atacante gana');
+               ELSE
+                  dbms_output.put_line('La defensa gana');   
+              END IF;
           ELSE 
                dbms_output.put_line('El ataque no es posible');
           END IF;
-         exit when curataque%notfound;
+         
       END LOOP;
+    close curataque;
 END;
 /
