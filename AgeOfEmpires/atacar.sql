ca8462a32f3bb05d452cc2a10597cafdb9d7c521
adriancr93 <aobregonr@ucenfotec.ac.cr>
2018-08-12 00:15:43 -0600

    Corregido

--- AgeOfEmpires/atacar.sql
+++ AgeOfEmpires/atacar.sql
@@ -6,6 +6,7 @@ IS
   subTotalDefensa number :=0;
   totalAtaque number := 0;
   totalDefensa number := 0;
+
   CURSOR curataque(identAtaque int) IS
      SELECT cantOro, puntosAtaque, puntosDefensa 
      FROM reino r JOIN tesoro t ON r.idreino = t.idtesoro 
@@ -13,6 +14,7 @@ IS
      cantoro tesoro.cantOro%type;
      puntosataque reino.puntosAtaque%type;
      puntosdefensa reino.puntosDefensa%type;
+
   CURSOR curdefensa(identDefensa int) IS 
       SELECT cantOro, cantMadera, cantHierro, puntosAtaque, puntosDefensa
       FROM reino r JOIN tesoro t On r.idreino = t.idtesoro
@@ -25,7 +27,7 @@ IS
 BEGIN
      open curataque(reinoAtacante);
       LOOP
-         fetch curataque into cantoro, puntosataque, puntosataque;
+         fetch curataque into cantoro, puntosataque, puntosdefensa;
          exit when curataque%notfound;
           IF cantoro >= 1000 then
                UPDATE tesoro
@@ -34,7 +36,8 @@ BEGIN
                subTotalAtaque :=  puntosataque * 80 / 100;
                subTotalAtaqueBonus := puntosataque * 60 / 100;
                totalAtaque := subTotalAtaque + subTotalAtaqueBonus;
-               dbms_output.put_line(totalAtaque);
+               dbms_output.put_line(subTotalAtaqueBonus);
+               dbms_output.put_line(subTotalAtaque);
                open curdefensa(reinoDefensa);
                 LOOP
                    fetch curdefensa into cantorodef, cantmaderadef, canthierrodef, puntosataquedef, puntosdefensadef;
