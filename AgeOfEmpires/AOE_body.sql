39e325b3f6165447beb22e5c141b215e7362518d
christianAle <cristr97@gmail.com>
2018-08-21 12:54:18 -0600

    exception en mejorar defensa y ataque

--- AgeOfEmpires/AOE_body.sql
+++ AgeOfEmpires/AOE_body.sql
@@ -306,79 +306,97 @@
         END;
         
         procedure mejorarDefensa(nomReino varchar2 ) is
-           reinoid number(15);
-           nombreRei  varchar2(200):=nomReino; 
-           cursor rein is
-           select tesoro_idtesoro tesoro,reservacentral_idreserva reserva
-           from reino
-           where UPPER(nombre) = UPPER(nombreRei); 
-        begin
-          select idreino into reinoid
+          error Exception;   
+          reinoid number(15);
+          nombreRei  varchar2(200):=nomReino; 
+          cursor rein is
+          select tesoro_idtesoro tesoro,reservacentral_idreserva reserva,t.cantoro cant
           from reino
+          join tesoro t on (t.idtesoro=reino.tesoro_idtesoro)
           where UPPER(nombre) = UPPER(nombreRei); 
-          for def in rein loop
-            update tesoro
-            set cantoro=cantoro-2000,
-            cantmadera=cantmadera-100,
-            canthierro=canthierro-150
-            where idtesoro=def.tesoro;
-        
-            update reservacentral
-            set cantoro=cantoro+2000,
-            cantmadera=cantmadera+100,
-            canthierro=canthierro+150
-            where idreserva=def.reserva;
-        
-            insert into bitacora values (bitacora_sequence.nextval,sysdate,2000,150,100,40,'M+D',reinoid);
-        
-            update reino
-            set puntosdefensa=puntosdefensa * 10/100+500,
-            cantcoronas=cantcoronas +40
-            where idreino =reinoid;
-          end loop;
-        Exception
-          when no_data_found 
-          then DBMS_OUTPUT.PUT_LINE('El reino no se encontro');
-        
-        end mejorarDefensa;
-        
+       begin
+        select idreino into reinoid
+        from reino
+        where UPPER(nombre) = UPPER(nombreRei); 
+       for def in rein loop
+               
+       if def.cant< 2000 then
+
+        raise error; 
+    
+       else 
+        update tesoro
+        set cantoro=cantoro-2000,
+        cantmadera=cantmadera-100,
+        canthierro=canthierro-150
+        where idtesoro=def.tesoro;
+
+        update reservacentral
+        set cantoro=cantoro+2000,
+        cantmadera=cantmadera+100,
+        canthierro=canthierro+150
+        where idreserva=def.reserva;
+
+        insert into bitacora values (bitacora_sequence.nextval,sysdate,2000,150,100,40,'M+D',reinoid);
+
+        update reino
+         set puntosdefensa=puntosdefensa * 10/100+500,
+         cantcoronas=cantcoronas +40
+         where idreino =reinoid;
+       end if;
+      end loop;
+    Exception
+     when no_data_found 
+     then DBMS_OUTPUT.PUT_LINE('El reino no se encontro');
+     when error then 
+     DBMS_OUTPUT.PUT_LINE('El reino no tiene sufiente oro');
+  end mejorarDefensa;
+
         procedure mejorarAtaque(nomReino varchar2 ) is
-           reinoid number(15);
-           nombRei varchar2(200):=nomReino;
-           cursor rein is
-           select tesoro_idtesoro tesoro,reservacentral_idreserva reserva
-           from reino
-           where UPPER(nombre) = UPPER(nombRei);
-        begin
-          select idreino into reinoid
-          from reino
-          where UPPER(nombre) = UPPER(nombRei);
-          
-          for def in rein loop
-            update tesoro
-            set cantoro=cantoro-1500,
-            cantmadera=cantmadera-300,
-            canthierro=canthierro-200
-            where idtesoro=def.tesoro;
-        
-            update reservacentral
-            set cantoro=cantoro+1500,
-            cantmadera=cantmadera+300,
-            canthierro=canthierro+200
-            where idreserva=def.reserva;
-        
-            insert into bitacora values (bitacora_sequence.nextval,sysdate,1500,300,200,5,'M+A',reinoid);
-            update reino
-            set puntosdefensa=puntosdefensa * 10/100+500,
-            cantcoronas=cantcoronas +40
-            where idreino =reinoid;
-        
-          end loop;
-        Exception
-          when no_data_found 
-          then DBMS_OUTPUT.PUT_LINE('El Reino no se encontro');
-         
-        end mejorarAtaque;
+         error Exception;   
+         reinoid number(15);
+         nombRei varchar2(200):=nomReino;
+         cursor rein is
+         select tesoro_idtesoro tesoro,reservacentral_idreserva reserva,t.cantoro cant
+         from reino
+         join tesoro t on (t.idtesoro=reino.tesoro_idtesoro)
+         where UPPER(nombre)=UPPER(nombRei);
+       begin
+        select idreino into reinoid
+        from reino
+        where UPPER(nombre) = UPPER(nombRei);
+  
+       for def in rein loop
+        if def.cant< 1500 then
+      
+          raise error; 
+    
+        else   
+          update tesoro
+          set cantoro=cantoro-1500,
+          cantmadera=cantmadera-300,
+          canthierro=canthierro-200
+          where idtesoro=def.tesoro;
+
+         update reservacentral
+         set cantoro=cantoro+1500,
+         cantmadera=cantmadera+300,
+         canthierro=canthierro+200
+         where idreserva=def.reserva;
+
+         insert into bitacora values (bitacora_sequence.nextval,sysdate,1500,300,200,5,'M+A',reinoid);
+         update reino
+         set puntosdefensa=puntosdefensa * 10/100+500,
+         cantcoronas=cantcoronas +40
+         where idreino =reinoid;
+         end if;
+        end loop;
+       Exception
+        when no_data_found 
+        then DBMS_OUTPUT.PUT_LINE('El Reino no se encontro');
+        when error then 
+        DBMS_OUTPUT.PUT_LINE('El reino no tiene sufiente oro');
+      end mejorarAtaque;
         
         PROCEDURE atacar
          ( reinoAtacante int, reinoDefensa int)
--- AgeOfEmpires/procedures/mejorarAta.sql
+++ AgeOfEmpires/procedures/mejorarAta.sql
@@ -1,9 +1,11 @@
 create or replace procedure mejorarAtaque(nomReino varchar2 ) is
+    error Exception;   
    reinoid number(15);
    nombRei varchar2(200):=nomReino;
    cursor rein is
-   select tesoro_idtesoro tesoro,reservacentral_idreserva reserva
+   select tesoro_idtesoro tesoro,reservacentral_idreserva reserva,t.cantoro cant
    from reino
+   join tesoro t on (t.idtesoro=reino.tesoro_idtesoro)
    where UPPER(nombre)=UPPER(nombRei);
 begin
   select idreino into reinoid
@@ -11,6 +13,11 @@ begin
   where UPPER(nombre) = UPPER(nombRei);
   
   for def in rein loop
+    if def.cant< 1500 then
+      
+     raise error; 
+    
+    else   
     update tesoro
     set cantoro=cantoro-1500,
     cantmadera=cantmadera-300,
@@ -28,11 +35,12 @@ begin
     set puntosdefensa=puntosdefensa * 10/100+500,
     cantcoronas=cantcoronas +40
     where idreino =reinoid;
-
+    end if;
   end loop;
 Exception
   when no_data_found 
   then DBMS_OUTPUT.PUT_LINE('El Reino no se encontro');
- 
+ when error then 
+   DBMS_OUTPUT.PUT_LINE('El reino no tiene sufiente oro');
 end mejorarAtaque;
 /
