e87e906915b9429136e1b98118b02eb77925752e
CHRISTIANPC\Christian <ccamposcr@gmail.com>
2018-08-20 23:16:55 -0600

    comprar added

--- AgeOfEmpires/AOE.sql
+++ AgeOfEmpires/AOE.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE PACKAGE AOE IS
 	PROCEDURE inicializar ( idReino int, oro int, madera int, hierro int, coronas int);
+	PROCEDURE comprar ( recurso varchar2 , reino varchar2, cantidad float );
 	PROCEDURE vender ( recurso varchar2 , reino varchar2, cantidad float );
 	PROCEDURE entrenarEjercitos ( tropa varchar2 , reino varchar2, cantidad float );
 	PROCEDURE comprarDefensas ( defensa varchar2 , reino varchar2, cantidad float );
